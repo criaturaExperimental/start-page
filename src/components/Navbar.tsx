dd6ee10c659dd887ca5173f2099ed7c9218d7a74
ldegomez <lucas.gomez@lastminute.com>
2022-01-07 12:10:04 +0100

    Enable css prop

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,25 @@
 import Link from "next/link";
+import styled from 'styled-components';
+
+const NavStyled = styled.nav`
+  width: 100%;
+  height: 4rem;
+  background: papayawhip;
+`;
 
 export default function NavBar() {
   return (
-    <>
+    <NavStyled
+      css={`
+        color: red;
+      `}
+    >
       <Link href="/">
         <a>home</a>
       </Link>
       <Link href="/about">
         <a>about</a>
       </Link>
-    </>
+    </NavStyled>
   );
 }
--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import type {} from 'styled-components/cssprop';
 
 export const GlobalStyle = createGlobalStyle`
   html,
