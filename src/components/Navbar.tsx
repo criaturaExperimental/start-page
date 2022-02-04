03537ed69b9163d0f258775b7e8537828d75b595
ldegomez <lucas.gomez@lastminute.com>
2022-02-04 12:15:20 +0100

    using emotion

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,19 @@
 import Link from "next/link";
+import { css } from '@emotion/react';
 
 export default function NavBar() {
   return (
-    <>
+    <div
+    css={css`
+      background-color: hotpink;
+    `}
+  >
       <Link href="/">
         <a>home</a>
       </Link>
       <Link href="/about">
         <a>about</a>
       </Link>
-    </>
+    </div>
   );
 }
