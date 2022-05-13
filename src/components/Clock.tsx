229909042b92c955aefc8f74f539213ca887b2ed
ldegomez <lucas.gomez@lastminute.com>
2022-05-13 09:29:36 +0200

    Clock component

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,5 +1,24 @@
+import styled from '@emotion/styled';
+import { format } from 'date-fns';
+import { useEffect, useState } from 'react';
+
+const ClockStyled = styled.span`
+  font-size: 7em;
+  color: white;
+`;
+
 export function Clock() {
-  return (
-    <div>Clock</div>
-  )
+
+  const [now, setNow] = useState(new Date())
+  const HMS = format(now, 'hh:mm:ss');
+
+  useEffect(() => {
+    setInterval(() => {
+      setNow(new Date());
+    }, 1000)
+
+  }, [])
+
+
+  return <ClockStyled>{HMS}</ClockStyled>;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,26 @@
 import type { NextPage } from 'next';
 import { css } from '@emotion/react';
-import Layout from '../components/Layout';
 import { useTheme } from '@emotion/react';
+import { Clock } from '../components/Clock';
 
 const Home: NextPage = () => {
   const theme = useTheme();
 
   return (
-    <Layout>
-      <div
-        css={css`
-          height: 600px;
-          background-color: ${theme.color.light};
-        `}
-      >
-        Hello world
-      </div>
-    </Layout>
+    <main
+      css={css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-shadow: 4px 5px 8px black;
+        height: 100vh;
+        background-position: center;
+        background-size: cover;
+        background-image: url('https://images.unsplash.com/photo-1650346910623-3a0d9ee1f2ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2342&q=80');
+      `}
+    >
+      <Clock />
+    </main>
   );
 };
 
